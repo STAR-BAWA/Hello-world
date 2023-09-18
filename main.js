b779a3a712109a1821b3e616d0c4ba0f7a6f4d5e
STAR BAWA <saistarbawa@gmail.com>
2023-09-18 19:47:38 +0530

    This commits all the files 2

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function main(){
-  document.getElementById().onclick(clickme);   
+function main() {
+  // Get all elements with the class "div"
+  var elements = document.getElementsByClassName("div");
+
+  // Loop through the elements and attach a click event listener to each
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function() {
+      alert("This is an alert");
+    });
+  }
 }
 
-function clickme(){
-
-    console.log("hello Git Javascript ")
-
-}
+main();
