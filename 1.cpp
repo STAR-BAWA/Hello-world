9b89294ff1b0f3d0e458cf9bf25d9156ccd81614
STAR BAWA <saistarbawa@gmail.com>
2023-09-19 18:59:26 +0530

    commiting cpp on local branch

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+
+    cout<<"Enter a number2"<<endl;
+    cin>>b;
+
+    cout<<"the Sum of a and b is"<<(a+b);
+
+}
